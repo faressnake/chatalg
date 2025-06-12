41cf56bf679e9462974994bf9de856b3891e4979
faressnake <132618855+faressnake@users.noreply.github.com>
2025-06-12 01:39:40 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("chatdz-cache").then(cache => {
+      return cache.addAll(["chat.html", "manifest.json", "icon-192.png", "icon-512.png"]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
